9c176e96e3320b7d07148c34ee041e39461fcacb
yudai1996 <72481372+yudai1996@users.noreply.github.com>
2020-10-28 19:58:07 +0900

    バリデーションの設定

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,4 +2,11 @@ class Prototype < ApplicationRecord
   belong_to :users
   has_many  :comments
   has_one_attached :image
+
+  #バリデーションの設定
+  validates :image,      presence: true
+  validates :title,      presence: true
+  validates :catch_copy, presence: true
+  validates :concept,    presence: true
+
 end
