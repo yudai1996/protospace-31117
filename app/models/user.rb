55041385611024db83ad57178a70662f46ced23b
yudai1996 <72481372+yudai1996@users.noreply.github.com>
2020-10-28 14:34:03 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #バリデーションの設定（email,passはデフォルト）
+  validates :name,       presence: true
+  validates :profile,    presence: true
+  validates :occupation, presence: true
+  validates :position,   presence: true
+
 end
