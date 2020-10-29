3bb3480f6bf1d97fcb633a6776e95d3e3c32904a
yudai1996 <72481372+yudai1996@users.noreply.github.com>
2020-10-30 02:06:04 +0900

    updateアクションの処理設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -26,6 +26,13 @@ class PrototypesController < ApplicationController
   end
 
   def update
+    prototype = Prototype.find(params[:id])
+    
+    if prototype.update(prototype_params)
+      redirect_to prototype_path(prototype.id)
+    else
+      render :edit 
+    end
   end
 
   private
