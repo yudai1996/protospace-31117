86fac02b72edbd630878c50529186ce6f63d12a1
yudai1996 <72481372+yudai1996@users.noreply.github.com>
2020-10-30 03:36:54 +0900

    ストロングパラメーターの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,10 @@ class CommentsController < ApplicationController
 
   def create
   end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:comment).merge(user_id: current_user.id, prototype_id: current_user.id)
+    end
+
 end
