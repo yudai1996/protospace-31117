c880377fc0080658efa4cea465b3c71c1f60ac67
yudai1996 <72481372+yudai1996@users.noreply.github.com>
2020-10-29 12:38:16 +0900

    ストロングパラメーターの設定/user_idのmerge

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -10,7 +10,9 @@ class PrototypesController < ApplicationController
   def create
   end
 
-
-
+  private
+    def prototype_params
+      params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+    end
 
 end
