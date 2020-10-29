039769e5bd79501f257cbebbe06094a18b082216
yudai1996 <72481372+yudai1996@users.noreply.github.com>
2020-10-29 16:39:38 +0900

    誤字修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
 
-  resources :prototypes, only: [:new, :creste]
+  resources :prototypes, only: [:new, :create]
 end
