870a7fe1e279e7f185a3e653ad1c4bb34f3f8ada
yudai1996 <72481372+yudai1996@users.noreply.github.com>
2020-10-30 11:30:17 +0900

    コメント機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,16 @@
 class CommentsController < ApplicationController
-
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to prototype_path(@comment.prototype)
+    else
+      redirect_to prototype_path(@comment.prototype)
+    end
   end
 
   private
-    def comment_params
-      params.require(:comment).permit(:comment).merge(user_id: current_user.id, prototype_id: current_user.id)
-    end
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
 
 end
