fcefb5665517144239924dd0bcf00e8b019ed291
yudai1996 <72481372+yudai1996@users.noreply.github.com>
2020-10-30 13:52:39 +0900

    部分テンプレート呼び出しに関する、修正兼試行錯誤

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,13 +4,15 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
-      redirect_to prototype_path(@comment.prototype)
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
+      render "prototypes/show"
     end
   end
 
   private
+
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
-
 end
