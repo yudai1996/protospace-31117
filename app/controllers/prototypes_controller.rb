fe1dc2850aaafd8708ccd18902db997a10a6601f
yudai1996 <72481372+yudai1996@users.noreply.github.com>
2020-10-30 14:26:37 +0900

    Herokuでエラーが出た為修正、見た目の整理

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
-
   def index
     @prototypes = Prototype.all
   end
@@ -27,33 +26,29 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-
     unless current_user == @prototype.user
       redirect_to root_path
-      
+    end
   end
 
   def update
     @prototype = Prototype.find(params[:id])
-    
     if @prototype.update(prototype_params)
-      redirect_to prototype_path(prototype)
+      redirect_to prototype_path(@prototype)
     else
-      render :edit 
+      render :edit
     end
   end
 
   def destroy
     prototype = Prototype.find(params[:id])
-    if prototype.destroy
-      redirect_to root_path
-    end
+    prototype.destroy
+    redirect_to root_path
   end
 
   private
-  
-    def prototype_params
-      params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-    end
 
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
 end
